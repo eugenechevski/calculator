a85a64d312b9b358ba79d8f624a75c5548d24b06
Yauheni Khvashcheuski <evgenijhq@gmail.com>
2021-10-18 14:50:38 -0400

    04. Added styles.

--- scripts/script.js
+++ scripts/script.js
@@ -5,65 +5,83 @@ const controlsContent = [
     ['1', '2', '3', '-'],
     ['0', '.', '=']
 ]
-
 const controlsContainer = document.getElementById('controls');
-// # DEBUG
-console.log(controlsContainer);
+var controlElements = [document.getElementById('screen')];
+
+
 
 // Draw the calculator
-document.body.onload = () => {
-    for (let i = 0; i < controlsContent.length; i++) {
-        for (let j = 0; j < controlsContent[i].length; j++) {
-            // Construct the element
-            // Things to include:
-            // 1. class attribute value
-            // 2. event listener
-            // 3. ...
-            let control = document.createElement('button');
-            let classValue = 'btn ';
-            
-            // Determine what type of column it is
-            if (controlsContent[i][j] === '0') {
-                classValue += 'col-6';
+for (let i = 0; i < controlsContent.length; i++) {
+    for (let j = 0; j < controlsContent[i].length; j++) {
+        // Construct the element
+        // Things to include:
+        // 1. class attribute value
+        // 2. event listener
+        // 3. group id
+        let control = document.createElement('button');
+
+        // Determine what type of control it is
+        // and add a corresponding class, so we can
+        // select by that class in CSS
+        if (j === controlsContent[i].length - 1) {
+            // Only if it's the last iteration 
+            // of the inner loop
+            control.id = 'operators-2';
+        } else {
+            // The first 3 will always have the 'digits' class
+            // except when it's the first iteration and
+            // in that case, it's the 'operators-1' class
+            if (i === 0) {
+                control.id = 'operators-1';
             } else {
-                classValue += 'col'
+                control.id = 'digits';
             }
-
-            classValue += ' ';
-            
-            // Determine what type of control it is
-            // and add a corresponding class, so we can
-            // select by that class in CSS
-            if (j === controlsContent.length - 1) {
-                // Only if it's the last iteration 
-                // of the inner loop
-                classValue += 'operators-2';
-            } else {
-                // The first 3 will always have the 'digits' class
-                // except when it's the first iteration and
-                // in that case, it's the 'operators-1' class
-                if (i === 0) {
-                    classValue += 'operators-1';
-                } else {
-                    classValue += 'digits';
-                }
-            }
-
-            // Add the class value
-            control.className = classValue;
-
-            // Add the text content
-            control.textContent = controlsContent[i][j];
-
-            // Append the element to the container
-            controlsContainer.appendChild(control);
         }
+
+        // Construct the class
+        let classValue = 'btn';
+
+        classValue += ' ';
+
+        // Determine what type of column it is
+        if (controlsContent[i][j] === '0') {
+            classValue += 'col-6';
+        } else {
+            classValue += 'col'
+        }
+
+        classValue += ' '
+
+        // Add the theme
+        classValue += 'theme-1';
+
+        // ...
+
+        // Set the class value
+        control.className = classValue;
+
+        // Set the text content
+        control.textContent = controlsContent[i][j];
+
+        // # DEBUG
+        console.log(control);
+
+        // Store the element for further use
+        controlElements.push(control);
+
+        // Append the element to the container
+        controlsContainer.appendChild(control);
     }
-};
+}
 
-// # DEBUG
-console.log(controlsContainer);
 
+// Helper for setting a theme
+function setTheme(themeName) {
+    controlElements.forEach(control => {
+        // Remove the last theme-2
+        control.className.replace('theme-\d+', themeName);
+    });
+}
 
 
 
