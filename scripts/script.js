4c96a19e17dedb7f1adeda4efaa6382c17b6231a
Yauheni Khvashcheuski <evgenijhq@gmail.com>
2021-10-25 13:21:46 -0400

    06. Implemented the half of the functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,25 @@
-/**
- * 
- *  Algorithm
- * 
- *  1. Create a string variable to store a display result
- *  
- * 
- *  2. Read the user's input
- * 
- *     There are two types of inputs: 
- *          1. Operands(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.');
- *          2. Operators(/, *, +, -, =, C, +/-, %);
- * 
- *     If the type of an input is operand:
- *          if (input is '.' and display result's last character is '.') or (input is '0' and display result is '0') :
- *              return
- *          else:
- *              if display result is '0':
- *                  display result = ''
- *              display result += input
- *  
- *     ...
- *  
- * 
- * 
- *      
- *  
- * 
- */
+// Grab the elements
 
+const displayContainer = document.getElementById('result-display');
+const controlsContainer = document.getElementById('controls');
+const themeSwitches = document.getElementsByClassName('theme-switch');
+
+// Variables and constants
 
 const controlsContent = [
     ['C', '+/-', '%', '/'],
     ['7', '8', '9', '*'],
     ['4', '5', '6', '+'],
     ['1', '2', '3', '-'],
-    ['0', '.', '=']
+    ['0', '.', '='],
 ]
-const controlsContainer = document.getElementById('controls');
-const themeSwitches = document.getElementsByClassName('theme-switch');
-var controlElements = [document.getElementById('screen')];
+
+var controlElements = {
+    'screen':  document.getElementById('screen'),
+};
+var displayResult = '';
+var displayFontSize = 2;
+var canOperate = true;
 
 
 // Draw the calculator
@@ -50,6 +31,11 @@ for (let i = 0; i < controlsContent.length; i++) {
         // 2. event listener
         // 3. group id
         let control = document.createElement('button');
+        let controlType = '';
+        let controlName = controlsContent[i][j];
+        
+        // Construct the class
+        let classValue = '';
 
         // Determine what type of control it is
         // and add a corresponding class, so we can
@@ -57,25 +43,24 @@ for (let i = 0; i < controlsContent.length; i++) {
         if (j === controlsContent[i].length - 1) {
             // Only if it's the last iteration 
             // of the inner loop
-            control.id = 'operators-2';
+            classValue = 'operator';
         } else {
             // The first 3 will always have the 'digits' class
             // except when it's the first iteration and
             // in that case, it's the 'operators-1' class
             if (i === 0) {
-                control.id = 'operators-1';
+                classValue = 'modificator';
             } else {
-                control.id = 'digits';
+                classValue  = 'digit';
             }
         }
 
-        // Construct the class
-        let classValue = 'btn';
+        controlType = classValue;
 
-        classValue += ' ';
-
+        classValue += ' btn '
+        
         // Determine what type of column it is
-        if (controlsContent[i][j] === '0') {
+        if (controlName === '0') {
             classValue += 'col-6';
         } else {
             classValue += 'col'
@@ -86,21 +71,82 @@ for (let i = 0; i < controlsContent.length; i++) {
         // Add the theme
         classValue += 'theme-1';
 
-        // ...
-
         // Set the class value
         control.className = classValue;
 
         // Set the text content
-        control.textContent = controlsContent[i][j];
+        control.textContent = controlName;
+
+        // Add the event listener
+        control.addEventListener('click', () => {
+            // Forward the handling of the input to the handler
+            handleInput(controlType, controlName);
+        });
 
         // Store the element for further use
-        controlElements.push(control);
+        controlElements[control.textContent] = control;
 
         // Append the element to the container
         controlsContainer.appendChild(control);
     }
 }
+
+// Get ready to listen for events
+controlElements['0'].focus();
+
+// Add the keyboard-event listener
+
+let altIsPressed = false;
+controlsContainer.addEventListener('keydown', (e) => {
+    let keyValue = e.key.toUpperCase();
+
+    console.log(keyValue);
+
+    // Check for the key modifiers being pressed
+
+    if (keyValue === 'ALT') {
+        altIsPressed = true;
+        return;
+    }
+
+    if (keyValue === 'SHIFT') {
+        return;
+    }
+
+    // Check for the controls which have two keys
+    // which can invoke the control
+
+    if (keyValue === 'ENTER') {
+        keyValue = '=';
+    } else if (keyValue === 'ESCAPE') {
+        keyValue = 'C';
+    // A hidden control which doesn't have an associated button
+    // and has to be invoked manually
+    } else if (keyValue === 'BACKSPACE') {
+        keyValue = 'Remove';
+        handleInput('modificator', keyValue);
+    }
+
+    // Check for the special control '+/-, which can be triggered only with 
+    // the shortcut 'Alt(Windows) or Option(Mac)' key pressed and the '-' key.
+    if (altIsPressed && keyValue === '–') {
+        keyValue = '+/-';
+    }
+    
+    // Check if the key is associated with any of the controls
+    if (Object.keys(controlElements).includes(keyValue)) {
+        controlElements[keyValue].focus();
+        controlElements[keyValue].click();
+    }
+});
+
+// Add the key-event listener to reset the state of the flag 
+// when the key-modifier 'Alt(Windows) or Option(Mac)' is being released
+controlsContainer.addEventListener('keyup', (e) => {
+    if (e.key.toUpperCase() === 'ALT') {
+        altIsPressed = false;
+    }
+});
 
 // Add the event listener for the theme switches
 for (let i = 0; i < themeSwitches.length; i++) {
@@ -109,14 +155,120 @@ for (let i = 0; i < themeSwitches.length; i++) {
     });
 }
 
+// Handles a click event of any control
+function handleInput(controlTypeName, controlName) {
+     // Check the input type
+
+     if (controlTypeName === 'digit') {
+        operand(controlName);
+    } else if (controlTypeName === 'modificator') {
+        modify(controlName);
+    } else if (controlTypeName === 'operator') {
+        operate(controlName);
+    } else {
+        // Error
+    }
+
+    updateDisplay();
+}
+
+// Helper to read the input from 'digit' controls
+function operand(inputValue) {
+    // Check if the input is being made after an operation
+    if (canOperate === false) {
+        displayResult = '0';
+        canOperate = true;
+    }
+
+    // Check for attempting to input more than one '.' in a row
+    // Also check for attempting to input more than one leading zero
+    if (inputValue === '.' && displayResult.slice(-1) === '.' ||
+        inputValue === '0' && displayResult === '0') {
+            return;
+    } else {
+        // Check if the input is the first digit
+        if (displayResult === '0') {
+            displayResult = '';
+        }
+
+        // Append the input to the result
+        displayResult += inputValue;
+    }
+}
+
+// Helper to modify the result
+function modify(operation) {
+    switch (operation) {
+        case 'Remove':
+            // Can remove no more characters
+            if (displayResult === '0') {
+                // Pass
+            } else if (displayResult.length === 1) {
+                displayResult = '0';
+            } else {
+                displayResult = displayResult.slice(0, -1);
+            }
+
+            break;
+        case 'C':
+            // Handle the case
+            break;
+        case '+/-':
+            // Handle the case
+            break;
+        case '%':
+            // Handle the case
+            break;
+    }
+}
+
+// Helper to operate on the result
+function operate(operation) {
+    
+}
+
+// Helper which performs the calculation
+function calculate(input) {
+
+}
+
+// Helper which updates the display
+function updateDisplay() {
+    // Check for the length of the result displayed
+    // in order to dynamically fit the content
+
+    // Check if the max number of characters
+    // was reached, if yes reset the display and ...
+    if (displayResult.length === 50) {
+        displayResult = '0';
+    }
+
+    // Check if we need to reset the standdart font size
+    // when the number of characters is less than 16 again
+    if (displayResult.length <= 16 && displayFontSize < 2) {
+        displayFontSize = 2;
+        displayContainer.style.fontSize = displayFontSize + 'rem';
+    } 
+
+    // Check if the content fits on the display
+    // if not adjust the font size and check again
+    while (displayContainer.clientWidth > (controlElements['screen'].clientWidth * 0.9)) {
+        displayContainer.style.fontSize = displayFontSize + 'rem';
+        displayFontSize -= 0.01;
+    }
+
+    displayContainer.textContent = displayResult;
+}
+
 // Helper for setting a theme
 function setTheme(themeName) {
-    controlElements.forEach(control => {
+    for (key in controlElements) {
+        let control = controlElements[key];
         // Replace the current theme with the new one
         let pattern = /theme-\d+/;
         let newClass = control.className.replace(pattern, themeName);
         control.className = newClass;
-    });
+    }
 }
 
 
